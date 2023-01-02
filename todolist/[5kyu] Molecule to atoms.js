04efa4c366910685682f55212f3eabc0c8aaade5
HazukiMiki <xzhou92@gmail.com>
2023-01-02 14:14:14 -0500

    yera

---
+++ todolist/[5kyu] Molecule to atoms.js
@@ -0,0 +1,12 @@
+/**
+ For a given chemical formula represented by a string, count the number of atoms of each element contained in the molecule and return an object (associative array in PHP, Dictionary<string, int> in C#, Map<String,Integer> in Java).
+
+var water = 'H2O';
+parseMolecule(water); // return {H: 2, O: 1}
+
+var magnesiumHydroxide = 'Mg(OH)2';
+parseMolecule(magnesiumHydroxide); // return {Mg: 1, O: 2, H: 2}
+
+var fremySalt = 'K4[ON(SO3)2]2';
+parseMolecule(fremySalt); // return {K: 4, O: 14, N: 2, S: 4}
+*/
